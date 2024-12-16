aa55a348e9ba59db5ec2205c12ad633c906ba8c4
JasonLino <jglino@student.apc.edu.ph>
2024-12-16 15:06:53 +0800

    Add AFOS create button

---
+++ app/(routes)/admin/manage-courses/_components/afos-dialog-button.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+export default function AFOSDialogButton() {
+  return (
+    <Button>
+      <Plus />
+      <span>Add</span>
+    </Button>
+  );
+}
---
+++ app/(routes)/admin/manage-courses/page.tsx
@@ -0,0 +1,11 @@
+import AFOSDialogButton from "./_components/afos-dialog-button";
+
+export default function ManageCoursesPage() {
+  return (
+    <div>
+      <div>
+        <AFOSDialogButton />
+      </div>
+    </div>
+  );
+}
