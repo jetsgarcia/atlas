0253782a618033d720733e03cf32ab80e81df4c4
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-12-16 15:26:46 +0800

    Add page title

--- app/(routes)/admin/manage-courses/page.tsx
+++ app/(routes)/admin/manage-courses/page.tsx
@@ -2,8 +2,11 @@ import AFOSDialogButton from "./_components/afos-dialog-button";
 
 export default function ManageCoursesPage() {
   return (
-    <div>
-      <div>
+    <div className="pt-10 max-w-[80rem] m-auto">
+      <div className="flex justify-between items-center"> 
+        <h1 className="text-xl font-semibold">
+          AFOS &#40;Armed Forces Occupational Specialty&#41;
+        </h1>
         <AFOSDialogButton />
       </div>
     </div>
