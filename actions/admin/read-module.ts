fbf1eb21ec75fea7d11e2f1d74183ee0e23d0c1c
Jetter <jlgarcia2@student.apc.edu.ph>
2024-12-17 15:46:57 +0800

    Add server action to retrieve all module records from the database

---
+++ actions/admin/read-module.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { getDatabaseConnection } from "@/lib/db";
+
+export default async function ReadAllModules() {
+  const sql = getDatabaseConnection();
+
+  try {
+    const records = await sql`SELECT * FROM Modules`;
+    if (records.length === 0) {
+      return { success: false, message: "No records found" };
+    }
+    return {
+      success: true,
+      data: records,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
+  }
+}
