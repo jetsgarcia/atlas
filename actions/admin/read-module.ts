8429eb6f8ae43f37b121f3d5008711d660448261
Jetter <jlgarcia2@student.apc.edu.ph>
2024-12-18 08:42:57 +0800

    Add server action to retrieve all module records from the database

---
+++ actions/admin/read-module.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { getDatabaseConnection } from "@/lib/db";
+
+export default async function ReadModules({
+  afos_code,
+}: {
+  afos_code: string;
+}) {
+  const sql = getDatabaseConnection();
+
+  try {
+    const records =
+      await sql`SELECT * FROM Modules WHERE afos_code = ${afos_code}`;
+    if (records.length === 0) {
+      return { success: false, message: "No records found" };
+    }
+    return {
+      success: true,
+      data: records,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
+  }
+}
