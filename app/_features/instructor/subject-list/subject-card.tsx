c4519a7e39b1022d0cfc5fb71c4e1671b38665f7
Wayne Dizon <122617930+waynedizon@users.noreply.github.com>
2025-02-06 14:30:57 +0800

    Fixed syntax bug

--- app/_features/instructor/subject-list/subject-card.tsx
+++ app/_features/instructor/subject-list/subject-card.tsx
@@ -15,8 +15,8 @@ export default function SubjectCard({
       className="inline-block h-full w-[400px]"
     >
       <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200 flex flex-col h-full transition transform hover:shadow-lg hover:-translate-y-1 active:scale-95">
-        <div className="mb-2 h-40 w-full bg-darkGreen-400 rounded" />
-        <div className="flex flex-col flex-grow"
+        <div className="mb-2 h-40 w-full bg-darkGreen-400 rounded" >
+        <div className="flex flex-col flex-grow">
           <h3 className="text-lg font-semibold text-gray-900 leading-tight mb-2">
             {subjectName}
           </h3>
