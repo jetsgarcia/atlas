ef05ea07cb5f9fe768301235dcf65db61743cf74
Wayne Dizon <122617930+waynedizon@users.noreply.github.com>
2025-02-06 14:22:47 +0800

    Enhanced subject-list card design

--- app/_features/instructor/subject-list/subject-card.tsx
+++ app/_features/instructor/subject-list/subject-card.tsx
@@ -12,14 +12,16 @@ export default function SubjectCard({
   return (
     <Link
       href={`/instructor/subjects/${subjectCode}/videos`}
-      className="inline-block"
+      className="inline-block h-full w-[400px]"
     >
-      <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200 w-80 transition transform hover:shadow-lg hover:-translate-y-1 active:scale-95">
+      <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200 flex flex-col h-full transition transform hover:shadow-lg hover:-translate-y-1 active:scale-95">
         <div className="mb-2 h-40 w-full bg-darkGreen-400 rounded" />
-        <h3 className="text-lg font-semibold text-gray-900 leading-tight mb-2">
-          {subjectName}
-        </h3>
-        <p className="text-sm text-gray-600 mt-1">{subjectCode}</p>
+        <div className="flex flex-col flex-grow"
+          <h3 className="text-lg font-semibold text-gray-900 leading-tight mb-2">
+            {subjectName}
+          </h3>
+          <p className="text-sm text-gray-600 mt-1">{subjectCode}</p>
+        </div>
       </div>
     </Link>
   );
