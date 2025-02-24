d98547d6523442d2f10ecac9af1b8971f73bdd05
Jetter <jlgarcia2@student.apc.edu.ph>
2025-02-24 14:45:42 +0800

    Remove comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// Define metadata for the whole web app
 export const metadata: Metadata = {
   title: "Atlas",
   description: "An e-learning platform for the Philippine Army",
@@ -19,14 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        {/* Dependency for LDRS loader component */}
         <script
           type="module"
           defer
           src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/dotPulse.js"
         />
         <main>{children}</main>
-        {/* For pop-up messages */}
         <Toaster />
       </body>
     </html>
