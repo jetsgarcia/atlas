6cc28a584f7ea77b364fae8f63c9e8db3038ef6e
Jetter <jlgarcia2@student.apc.edu.ph>
2025-02-24 15:27:01 +0800

    Update loader component to use full height

--- components/loader.tsx
+++ components/loader.tsx
@@ -2,7 +2,7 @@ import type {} from "ldrs";
 
 export default function Loader() {
   return (
-    <div className="h-[40rem] grid place-items-center">
+    <div className="h-full grid place-items-center">
       <l-dot-pulse size="43" speed="1.5" color="darkGreen"></l-dot-pulse>
     </div>
   );
