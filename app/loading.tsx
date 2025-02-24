f85fd23d7dca55216e1bef38b0898f06d0fe020e
Jetter <jlgarcia2@student.apc.edu.ph>
2025-02-24 15:10:38 +0800

    Remove loading file

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import type {} from "ldrs";
-
-export default function Loading() {
-  return (
-    <div className="h-[40rem] grid place-items-center">
-      <l-dot-pulse size="43" speed="1.5" color="darkGreen"></l-dot-pulse>
-    </div>
-  );
-}
